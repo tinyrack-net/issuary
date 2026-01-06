49535325a79ff56fc04e7b9cf94abd77fc00a378
winetree94 <me@winetree94.com>
2026-01-06 22:38:21 +0900

    feat: update tsconfig for improved build and testing configurations

--- packages/backend/vitest.config.ts
+++ packages/backend/vitest.config.ts
@@ -3,4 +3,10 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    exclude: [
+      './dist/*',
+      './node_modules/*',
+    ]
+  },
 });
