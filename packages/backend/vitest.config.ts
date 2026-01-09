ff43ab3a988c3452f8f5116e3ed36139e7ab0af8
winetree94 <me@winetree94.com>
2026-01-09 23:59:32 +0900

    refactor: simplify vitest configuration by commenting out unused test options

--- packages/backend/vitest.config.ts
+++ packages/backend/vitest.config.ts
@@ -4,10 +4,7 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    reporters: 'verbose',
-    silent: false,
-    printConsoleTrace: true,
-    logHeapUsage: true,
+    // reporters: 'verbose',
     exclude: ['./dist/*', './node_modules/*'],
   },
 });
