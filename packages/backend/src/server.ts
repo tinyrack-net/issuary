1e6a753e09dea19f8292c29644e16988865083f8
winetree94 <me@winetree94.com>
2026-01-09 23:34:49 +0900

    refactor: improve code formatting and readability in server.ts

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -10,7 +10,9 @@ import { env } from '@/lib/env.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.resolve(path.dirname(__filename));
 
-export type FastifyWithZodInstance = Awaited<ReturnType<ReturnType<typeof createServer>['start']>>;
+export type FastifyWithZodInstance = Awaited<
+  ReturnType<ReturnType<typeof createServer>['start']>
+>;
 
 export function createServer() {
   const appInstance = Fastify({
@@ -18,9 +20,9 @@ export function createServer() {
       enabled: env.APP_ENV !== 'production',
       transport: {
         target: 'pino-pretty',
-        // options: {
-        //   colorize: true,
-        // },
+        options: {
+          colorize: true,
+        },
       },
     },
   }).withTypeProvider<ZodTypeProvider>();
@@ -30,19 +32,19 @@ export function createServer() {
       await appInstance.register(fastifyAutoload, {
         dir: path.join(__dirname, 'plugins'),
         ignorePattern: /(.+\.test|.spec)\.(ts|js)$/,
-      })
+      });
 
       await appInstance.register(fastifyAutoload, {
         dir: path.join(__dirname, 'services'),
         ignorePattern: /(.+\.test|.spec)\.(ts|js)$/,
-      })
+      });
 
       await appInstance.register(fastifyAutoload, {
         dir: path.join(__dirname, 'routes'),
         routeParams: true,
         autoHooks: true,
         options: {},
-        ignorePattern: /(.+\.test|.spec)\.(ts|js)$/
+        ignorePattern: /(.+\.test|.spec)\.(ts|js)$/,
       });
 
       if (env.APP_ENV !== 'test') {
@@ -58,14 +60,14 @@ export function createServer() {
       appInstance.log.error(err);
       process.exit(1);
     }
-  }
+  };
 
   const stop = async () => {
     await appInstance.close();
-  }
+  };
 
   return {
     start,
     stop,
-  }
+  };
 }
