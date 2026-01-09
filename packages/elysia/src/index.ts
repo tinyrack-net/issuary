10868d721379f70556b2a16af14d6d7eb5f9b601
winetree94 <me@winetree94.com>
2026-01-09 21:28:47 +0900

    chore: remove Elysia package and related files from the repository

--- packages/elysia/src/index.ts
+++
@@ -1,8 +0,0 @@
-import { node } from '@elysiajs/node';
-import { Elysia } from 'elysia';
-
-const app = new Elysia({ adapter: node() })
-  .get('/', () => 'Hello Elysia')
-  .listen(3001, ({ hostname, port }) => {
-    console.log(`🦊 Elysia is running at ${hostname}:${port}`);
-  });
