04ff46fe5bd8c9467b7e4f544369571dc60ee2ab
winetree94 <me@winetree94.com>
2026-01-23 02:37:44 +0900

    style: organize imports in frontend and remove unused preload link
    - Reorder imports in main.tsx to follow conventions
    - Remove preload for deprecated oauth/providers endpoint

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,8 +1,8 @@
-import { initI18n } from '@/i18n';
 import { QueryClientProvider, useSuspenseQueries } from '@tanstack/react-query';
 import { RouterProvider } from '@tanstack/react-router';
-import { StrictMode, memo, useEffect, useState } from 'react';
+import { memo, StrictMode, useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { initI18n } from '@/i18n';
 import './index.css';
 import { GlobalQueryClient } from './libs/query-client';
 import { AppRouter } from './libs/router';
