6f0e71eb1f7da1116c3877f029a20b6766f27488
winetree94 <me@winetree94.com>
2026-02-14 15:12:18 +0900

    refactor: register docs routes in route aggregation index

--- packages/backend/src/routes/index.ts
+++ packages/backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { createRouter } from '@backend/lib/create-router.js';
 import { wellKnownRoutes } from './.well-known/index.js';
+import { docsRoutes } from './api/docs/index.js';
 import { apiV1 } from './api/v1/index.js';
 import { oauthApplicationRoutes } from './application/oauth/index.js';
 
 export const routes = createRouter()
+  .route('/api', docsRoutes)
   .route('/api/v1', apiV1)
   .route('/application/oauth', oauthApplicationRoutes)
   .route('/.well-known', wellKnownRoutes);
